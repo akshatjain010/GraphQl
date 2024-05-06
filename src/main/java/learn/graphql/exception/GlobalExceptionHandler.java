257862eb7ecaee00e2c20295c135e2e87804942a
Lenovo <Lenovo@DESKTOP-GJ0O6HU>
2024-05-06 12:03:08 +0530

    'latest'

--- src/main/java/learn/graphql/exception/GlobalExceptionHandler.java
+++ src/main/java/learn/graphql/exception/GlobalExceptionHandler.java
@@ -5,35 +5,62 @@
 package learn.graphql.exception;
 
 import graphql.GraphQLError;
-import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import graphql.GraphqlErrorBuilder;
+import graphql.schema.DataFetchingEnvironment;
+import graphql.ErrorType;
 
-/**
- *
- * @author Akshat Jain
- */
+import org.springframework.graphql.execution.DataFetcherExceptionResolverAdapter;
+import org.springframework.stereotype.Component;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
-    
-    @GraphQlExceptionHandler
-    public GraphQLError handleBookNotFoundException(BookNotFoundException e) {    
-        return GraphQLError.newError()
-                .message(e.getMessage())
-                .build();
-    }
-    
-    @GraphQlExceptionHandler
-    public GraphQLError handleIllegalArgsException(IllegalArgsException e) {
-        return GraphQLError.newError()
-                .message(e.getMessage())
-                .build();
-    }
-    
-    @GraphQlExceptionHandler
-    public GraphQLError handleEmptyResultException(EmptyResultException e) {
-        return GraphQLError.newError()
-                .message(e.getMessage())
-                .build();
+//import org.springframework.graphql.data.method.annotation.GraphQlExceptionHandler;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//
+//@ControllerAdvice
+//public class GlobalExceptionHandler {
+//    
+//    @GraphQlExceptionHandler
+//    public GraphQLError handleBookNotFoundException(BookNotFoundException e) {    
+//        return GraphQLError.newError()
+//                .message(e.getMessage())
+//                .build();
+//    }
+//    
+//    @GraphQlExceptionHandler
+//    public GraphQLError handleIllegalArgsException(IllegalArgsException e) {
+//        return GraphQLError.newError()
+//                .message(e.getMessage())
+//                .build();
+//    }
+//    
+//    @GraphQlExceptionHandler
+//    public GraphQLError handleEmptyResultException(EmptyResultException e) {
+//        return GraphQLError.newError()
+//                .message(e.getMessage())
+//                .build();
+//    }
+//}
+
+@Component
+public class GlobalExceptionHandler extends DataFetcherExceptionResolverAdapter {
+
+    @Override
+    protected GraphQLError resolveToSingleError(Throwable ex, DataFetchingEnvironment env) {
+        if(ex instanceof BookNotFoundException || ex instanceof EmptyResultException) {
+            return GraphqlErrorBuilder.newError()
+              .errorType(ErrorType.DataFetchingException)
+              .message(ex.getMessage())
+              .path(env.getExecutionStepInfo().getPath())
+              .location(env.getField().getSourceLocation())
+              .build();
+        }
+        else if(ex instanceof IllegalArgsException) {
+            return GraphqlErrorBuilder.newError()
+              .errorType(ErrorType.ValidationError)
+              .message(ex.getMessage())
+              .path(env.getExecutionStepInfo().getPath())
+              .location(env.getField().getSourceLocation())
+              .build();
+        }
+        else return null;
     }
 }
